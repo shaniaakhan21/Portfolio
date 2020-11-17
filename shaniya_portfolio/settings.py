5384e40734b24e57458980309cda3cdbe493d1bb
shaniyakhan79 <shaniaakhan21@gmail.com>
2020-11-17 21:59:03 +0530

    env change

--- shaniya_portfolio/settings.py
+++ shaniya_portfolio/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 from decouple import config
 import django_heroku
+import environ
+environ.Env.read_env()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -22,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.environ[‘secret_key’]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
